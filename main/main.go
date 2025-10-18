a8b701a859264e0c5fdf4c99ecdac3a8c143da5b
Robson William <30180338+robwillup@users.noreply.github.com>
2025-10-18 11:04:30 -0300

    Add message capability

--- main/main.go
+++ main/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"gochatty/internal/auth"
+	"gochatty/internal/chat"
 	"gochatty/internal/db"
+	"gochatty/internal/websocket"
 	"log"
 	"os"
 
@@ -16,6 +18,15 @@ func main() {
 	r := gin.Default()
 	r.POST("/register", auth.Register)
 	r.POST("/login", auth.Login)
+
+	authorized := r.Group("/")
+	authorized.Use(auth.JWTAuthMiddleware())
+
+	authorized.POST("/messages", chat.PostMessage)
+	authorized.GET("/messages", chat.GetMessages)
+
+	go websocket.HandleMessages()
+	r.GET("/ws", websocket.HandleConnections)
 
 	log.Fatal(r.Run(":8080"))
 }
